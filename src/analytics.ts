5c90d5ac20ad5a3877825f9270882c88adec16c1
Cuh4a <cuh4a@yandex.ru>
2022-06-09 16:53:21 +0300

    feat: learning webpack

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,20 +1,20 @@
 import * as $ from "jquery"
 
 
-function createAnalytics () {
+function createAnalytics(): object {
     let counter = 0;
-    let destroyed = false;
+    let destroyed: boolean = false;
 
-    const listener = () => counter++;
+    const listener = (): number => counter++;
 
     $(document).off('click', listener)
 
     return {
-        destroy () {
+        destroy() {
             $(document).on('click', listener)
             destroyed = true;
         },
-        getClicks () {
+        getClicks() {
             if (destroyed) {
                 return `"Analytics is destroyed. Total clicks = ${counter}"`
             }
@@ -22,4 +22,4 @@ function createAnalytics () {
         }
     }
 }
-window.analytics = createAnalytics();
+window['analytics'] = createAnalytics();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const HTMLWebpackPugin = require("html-webpack-plugin"); //подключение webpack plugin HTML
-const {CleanWebpackPlugin} = require("clean-webpack-plugin"); //подключение webpack plugin clean
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin"); //подключение webpack plugin clean
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
-// const { config } = require("process");
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
@@ -28,18 +29,35 @@ const optimizaion = () => {
     return config;
 }
 
+const fileName = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+    const loaders = [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            publicPath: path.resolve(__dirname, 'dist')
+        },
+    }, 'css-loader']
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders;
+}
+
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
     mode: "development", //'это означает что мы все собираем в режиме разработки
     entry: { //две точки входа 
         // 1 точка входа:
-        main: "./index.js", //здесь подключаем главный файл js
+        main: ["@babel/polyfill", "./index.js"], //здесь подключаем главный файл js
         // 2 точка входа:
-        analytics: "./analytics.js"
+        analytics: "./analytics.ts"
     },
     output: {
-        filename: "[name].[contenthash].js", //общий файл в котором будут все файлы JS
+        filename: fileName('js'), //общий файл в котором будут все файлы JS
         path: path.resolve(__dirname, "dist") //путь куда будут сложены файлы
     },
     resolve: {
@@ -70,18 +88,13 @@ module.exports = {
             }]
         }),
         new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css", //общий файл в котором будут все файлы css
+            filename: fileName("css"), //общий файл в котором будут все файлы css
         })
     ],
     module: {
         rules: [{
                 test: /\.css$/, //для import с расширением css
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: path.resolve(__dirname, 'dist')
-                    },
-                }, 'css-loader']
+                use: cssLoaders(),
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
@@ -101,12 +114,33 @@ module.exports = {
             },
             {
                 test: /\.less$/, //для import с расширением less
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
+                use: cssLoaders("less-loader"),
+            },
+            {
+                test: /\.s[ac]ss$/, //для import с расширением Sass
+                use: cssLoaders("sass-loader"),
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
                     options: {
-                        publicPath: path.resolve(__dirname, 'dist')
-                    },
-                }, 'css-loader']
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.m?ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env','@babel/preset-typescript'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
             }
         ]
     }
