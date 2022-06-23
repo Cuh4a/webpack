1059c234bc668c27e1abe6368af822a3151c1ca1
Cuh4a <cuh4a@yandex.ru>
2022-06-23 15:53:45 +0300

    ready webpack HTML-layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import * as $ from "jquery" // подключение библиотеки jquery
 // import Post from "@models/post" //импортируем Post класс
 import "./styles/styles.css"
+import "./js/index2"
 // import json from "./assets/json"
 // import xml from "./assets/data.xml"
 // import csv from "./assets/data.csv"
